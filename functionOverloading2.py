218c4b97accd151b1aded4d3e2c7b36a1f29437c
KorchaginS <KorchaginSV1995@yandex.ru>
2022-07-19 06:12:35 +0300

    Перегрузка iter и next

--- functionOverloading2.py
+++ functionOverloading2.py
@@ -1,17 +1,71 @@
-class Squares:
-    def __init__(self, start, stop):
-        self.value = start - 1
-        self.stop = stop
+# class Squares:
+#     def __init__(self, start, stop):
+#         self.value = start - 1
+#         self.stop = stop
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.value == self.stop:
+#             raise StopIteration
+#         self.value += 1
+#         return self.value ** 2
+#
+# for i in Squares(1,10):
+#     print(i, end = ' ')
+
+
+class SkipObject:
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
 
     def __iter__(self):
-        return self
+        return SkipIterator(self.wrapped)
+
+
+class SkipIterator:
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        self.offset = 0
 
     def __next__(self):
-        if self.value == self.stop:
+
+        if self.offset >= len(self.wrapped):
             raise StopIteration
-        self.value += 1
-        return self.value ** 2
+        else:
+            item = self.wrapped[self.offset]
+            self.offset += 2
+            return item
+
+if __name__ == '__main__':
+
+    alpha = 'abcdef'
+    skipper = SkipObject(alpha)
+    I = iter(skipper)
+
+    print(next(I), next(I), next(I))
+
+    for x in skipper:
+        for y in skipper:
+
+            print(x + y, end=' ')
+
+class Squares:
+     def __init__(self, start, stop):
+         self.start = start - 1
+         self.stop = stop
+
+     def __iter__(self):
+         for value in range(self.start, self.stop + 1):
+             yield value ** 2
+
+     # def __next__(self):
+     #     if self.value == self.stop:
+     #         raise StopIteration
+     #     self.value += 1
+     #     return self.value ** 2
 
 for i in Squares(1,10):
     print(i, end = ' ')
-

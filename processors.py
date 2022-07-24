e33c1defb1dc4a9e12e191a11e1c98647e523521
KorchaginS <KorchaginSV1995@yandex.ru>
2022-07-24 21:54:38 +0300

    Реализовал абстрактный метода

--- processors.py
+++ processors.py
@@ -3,13 +3,23 @@ class Processor:
         self.reader = reader
         self.writer = writer
 
-    def Process(self):
+    def process(self):
         while True:
             data = self.reader.readline()
             if not data: break
-            data = self.converted(data)
+            data = self.converter(data)
             self.writer.write(data)
 
     def converter(self, data):
         assert False, 'converter must be defined'
+
+
+class Uppercase(Processor):
+    def converter(self, data):
+        return data.upper()
+
+if __name__ == '__main__':
+    import sys
+    obj = Uppercase(open('trispam.txt'), sys.stdout)
+    obj.process()
 

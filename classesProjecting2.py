03dbd3376fdf7530ab94a8b18c72bc7c21c3f541
KorchaginS <KorchaginSV1995@yandex.ru>
2022-07-27 21:45:03 +0300

    Связанные методы и их вызовы

--- classesProjecting2.py
+++ classesProjecting2.py
@@ -9,8 +9,45 @@ class C2:
 
 class C3(C1, C2): pass
 
-I = C3()
+# I = C3()
+#
+# I.meth1(); I.metha()
+# print(I.__dict__)
+# I.meth2(), I.methb()
 
-I.meth1(); I.metha()
-print(I.__dict__)
-I.meth2(), I.methb()
+def square (arg) :
+    return arg ** 2
+
+class Sum:
+    def __init__ (self, val) :
+        self.val = val
+
+    def __call__ (self, arg) :
+        return self.val + arg
+
+
+class Product:
+    def __init__ (self, val) :
+        self.val = val
+    def method (self, arg) :
+        return self.val * arg
+
+class Negate:
+    def __init__(self, val):
+        self.val= - val
+
+    def __repr__(self):
+        return str(self.val)
+
+sobject = Sum(2)
+pobject = Product(3)
+actions = [square, sobject, pobject.method, Negate]
+
+for act in actions:
+    print(act(5))
+
+
+
+
+
+

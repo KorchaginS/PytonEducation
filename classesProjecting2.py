8d98f8adc1f7ea70cebb4bf1e024a94c4209df79
KorchaginS <KorchaginSV1995@yandex.ru>
2022-07-29 17:22:17 +0300

    Update classesProjecting2.py

--- classesProjecting2.py
+++ classesProjecting2.py
@@ -62,10 +62,32 @@ class Spam(ListInstance):
     def __init__(self):
         self.data1 = 'food'
 
-if __name__ == '__main__':
+import importlib
 
-    x = Spam()
-    print(x)
+def tester(listerclass, sept=False) :
+    class Super:
+        def __init__ (self) :
+            self.datal = 'spam'
+        def ham(self) :
+            pass
+    class Sub(Super, listerclass):
+        def __init__ (self) :
+            Super.__init__ (self)
+            self.data2 = 'eggs'
+            self.data3 = 42
+        def spam(self):
+            pass
+
+    instance = Sub()
+    print(instance)
+    if sept: print('-' * 80)
+#
+#
+
+# if __name__ == '__main__':
+#
+#     # x = Spam()
+    # print(x)
 
 
 
